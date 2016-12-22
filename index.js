65ad4a3fee8c791fa2a722baae6135cf7e52c96c
iehouse <ie_kara_demasen@yahoo.co.jp>
2016-12-22 08:07:04 +0000

    htmlを追加してブラウザで動くように書き換え中

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 'use strict';
 
-let board = [0,1,2,3,4,5,6,7,8];
+let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const okPattern = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 let maru = [];
 let batsu = [];
 
-function gameSanmoku() {
-  //文字を並べてみる
-  selectNum('maru', 0);
-  selectNum('batsu', 4);
-  selectNum('maru', 2);
-  selectNum('batsu', 1);
-  selectNum('maru', 8);
-  selectNum('batsu', 7);                                                                                                                                                                                                                      
-  //描画
-  drawBoard();
-  //勝利判定
-  if(batsu.length >= 3) {
-    judgmentWin(maru);
-    judgmentWin(batsu);
-  }
-}
+const htmlboard = document.getElementById('board');
+const btnnum = document.getElementsByClassName('js_num');
+
+let playnum = 0;
+
+// game
+for (var i = 0; i < btnnum.length; i++) {
+  btnnum[i].addEventListener("click", function () {
+
+    let player = 'maru';
+    playnum++;
+
+    // プレイヤー切り替え
+    if (playnum % 2 === 0) {
+      player = 'batsu';
+    } else {
+      player = 'maru';
+    }
+
+    selectNum(player, this.dataset.number);
+
+    drawBoard();
+
+    if (maru.length >= 3) {
+      judgmentWin(maru);
+      judgmentWin(batsu);
+    }
+
+    // 引き分け
+    if (playnum === 9) {
+      console.log("引き分け！");
+    }
+
+  }, false);
+};
+
+//TODO 連続で同じ個所を押された時の処理
+
 
 
 /**
@@ -30,7 +52,7 @@ function gameSanmoku() {
  */
 function selectNum(role, number) {
   let sign = 'X';
-  if(role === 'maru') {
+  if (role === 'maru') {
     sign = 'O';
     maru.push(board[number]);
   } else {
@@ -47,15 +69,15 @@ function selectNum(role, number) {
  */
 function judgmentWin(rolearray) {
   let ok = 0;
-  for(let i = 0; i < okPattern.length; i++) {
-    
+  for (let i = 0; i < okPattern.length; i++) {
+
     // 勝利判定
-    if(ok === 3) {
+    if (ok === 3) {
       let role = 'maru';
-      if(rolearray === batsu) {
+      if (rolearray === batsu) {
         role = 'batsu';
       }
-      console.log("winner→ " + role );
+      console.log("winner→ " + role);
       ok = 0;
       break;
     } else {
@@ -63,8 +85,8 @@ function judgmentWin(rolearray) {
     }
 
     // パターン照合
-    for(let j = 0; j < okPattern[i].length; j++) {
-      if(rolearray.indexOf(okPattern[i][j]) >= 0) {
+    for (let j = 0; j < okPattern[i].length; j++) {
+      if (rolearray.indexOf(okPattern[i][j]) >= 0) {
         ok++;
       }
     }
@@ -76,13 +98,12 @@ function judgmentWin(rolearray) {
  * 盤上を描く
  */
 function drawBoard() {
-  console.log("------------");
-  for(let i = 0; i < board.length; i++) {
-   process.stdout.write(" " + board[i] + " |");
-   if( i === 2 || i === 5 || i === 8 ) {
-     console.log("\n------------");
-   }
+  for (let i = 0; i < board.length; i++) {
+    let div = document.getElementById(i);
+    if (board[i] === 'O' || board[i] === 'X') {
+      div.innerHTML = board[i];
+    } else {
+      div.innerHTML = " ";
+    }
   }
 }
-
-gameSanmoku();
